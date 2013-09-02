75ddb8cad26d0183d1e22cd252ff89e00b0f5e4f
tyler rinker <tyler_rinker@hotmail.com>
2013-09-02 15:33:21 -0400

    added first style.css

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ This is a collection of style.css templates and their corresponding output.
 
 Please feel free to upload a style.css template via: <a href="mailto:reports.rpackage@gmail.com" target="_blank">reports.rpackage@gmail.com</a> marked with **style.css template** in the subject line.
 
-To view the templates click here.
+## Examples
+
+To view the style template examples click below:
+
+[unstyled (default knitr)](https://github.com/trinker/style.css_examples/blob/master/minimal%template.Rmd)
+[rinker_vignette](http://trinker.github.io/style.css_examples/rinker_vignette/minimal%20template.html)
 
---
+++ management.R
@@ -0,0 +1,33 @@
+## file.exists("style.css examples/rinker_vignette")
+
+library(reports)
+fls <- system.file("extdata/style.css_library", package = "reports")
+x <- file.path(fls, dir(fls), "style.css")
+
+n <- 1
+file.exists("temp")
+x[n]
+
+library(reports)
+dirnm <- basename(gsub("/style.css", "", x[n]))
+xname <- folder(folder.name=file.path("style.css_examples", dirnm))
+file.copy(dir("temp"), xname, recursive = TRUE)
+cssname <- folder(folder.name=file.path(xname, "css"))
+file.copy(x[n], cssname, recursive = TRUE)
+
+library(knitr); library(markdown)
+knit2html(input=file.path(xname, "minimal template.Rmd"), output = file.path(xname, "minimal template.md"))
+markdownToHTML(file.path(xname, "minimal template.md"), file.path(xname, "minimal template.html"), 
+    stylesheet=file.path(xname, "css/style.css"))
+
+delete(file.path("C:/Users/trinker/GitHub/trinker.github.com", "style.css_examples"))
+folder(folder.name=file.path("C:/Users/trinker/GitHub/trinker.github.com", "style.css_examples"))
+## copy to website
+loc <- file.path(getwd(), "style.css_examples")
+fls2copy <- file.path(loc, dir(loc))
+file.copy(fls2copy, 
+		  file.path("C:/Users/trinker/GitHub/trinker.github.com", "style.css_examples"), recursive=TRUE)
+
+
+
+
